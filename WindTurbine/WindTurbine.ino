0f4de4bccccc16da75a001fb4e6d353c607b5e28
robert-csu <robert.oneil@colostate.edu>
2026-03-05 12:58:34 -0700

    Update WindTurbine.ino
    Updated the code to work standardly and rotate at a max of 25 rpm during the day and decrease it at night.

--- WindTurbine/WindTurbine.ino
+++ WindTurbine/WindTurbine.ino
@@ -1,20 +1,86 @@
-#include <Stepper.h>
+#include <AccelStepper.h>
+#include <IRremote.hpp>
 
-Stepper turbineArms(2048, 8,10,9,11);
+#define IR_RECEIVE_PIN 3
+
+#define OK 0xBF40FF00
+#define UP 0xB946FF00
+#define DOWN 0xEA15FF00
+#define LEFT 0xBB44FF00
+#define RIGHT 0xBC43FF00
+
+float currentSpeed = 853;
+bool step_flag = false;
+
+AccelStepper turbineArms(AccelStepper::HALF4WIRE, 8, 10, 9, 11);
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK); 
+  turbineArms.setMaxSpeed(1000);
+  Serial.println("Ready to receive IR signals...");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  checkSpeed();
+
+  if (IrReceiver.decode()) {
+    // This is the "void method" logic you requested
+    whatButtonCheck(IrReceiver.decodedIRData.decodedRawData);
+    IrReceiver.resume(); 
+  }
+
+  if(step_flag){
+    turbineArms.setSpeed(currentSpeed);
+    turbineArms.runSpeed();
+  }
+  else{
+    turbineArms.setSpeed(0);
+    turbineArms.stop(); 
+    turbineArms.disableOutputs();
+  }
+  
 }
 
-void checkSpeed(){
-  int currentSpeed = 10;//ifNight();
-  turbineArms.setSpeed(currentSpeed);
-  turbineArms.step(100);
+void whatButtonCheck(unsigned long code){
+
+  switch (code) {
+    case OK:
+      checkSTEPFLAG(); 
+      break;
+    case UP:
+      increaseSpeed();
+      break;
+    case DOWN:
+      decreaseSpeed();
+      break;
+    case LEFT:
+      // Stervo motor left
+      break;
+    case RIGHT:
+     // Stervo motor right
+    default:
+      // code to execute if variable does not match any case
+      break;
+  }
+}
+
+void checkSTEPFLAG(){
+  step_flag = !step_flag; 
+  if(step_flag) turbineArms.enableOutputs(); 
+  Serial.print("Turbine: ");
+  Serial.println(step_flag ? "ON (25 RPM)" : "OFF"); 
+}
+
+void increaseSpeed(){
+  currentSpeed += 50; 
+  Serial.print("Speed Increase: "); Serial.println(currentSpeed);
+}
+
+void decreaseSpeed(){
+  currentSpeed -= 50;
+  Serial.print("Speed down: "); Serial.println(currentSpeed);
 }
 
 /*
