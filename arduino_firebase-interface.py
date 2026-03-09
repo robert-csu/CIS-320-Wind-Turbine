a4d9c773763907eb715a63bdc4f204283c23b804
robert-csu <robert.oneil@colostate.edu>
2026-03-09 15:03:07 -0600

    Create arduino_firebase-interface.py (#41)

---
+++ arduino_firebase-interface.py
@@ -0,0 +1,16 @@
+import serial
+import firebase_admin
+from firebase_admin import credentials, db
+import datetime
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {"databaseURL": "https://windturbine-111c8-default-rtdb.firebaseio.com/"})
+ref = db.reference("WindTurbine/latest")
+
+arduino = serial.Serial('/dev/ttyUSB0', 9600)
+
+
+while True:
+    line = arduino.readline().decode().strip()
+    ldr = str(line)
+    ref.set({"LDR": ldr, "timeStamp": str(datetime.datetime.now())})
